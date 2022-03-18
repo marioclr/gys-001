2078ba8578d5cc2d8a6a3fa10908b355ffba35c4
Cesar Mario Lima Rodriguez <mariolr.mx@gmail.com>
2022-03-18 17:37:55 -0600

    Se realizaron configuraciones para acceso a base de datos Informix y clases de consulta de datos de Usuario

--- src/main/java/com/example/demo/controller/HomeController.java
+++ src/main/java/com/example/demo/controller/HomeController.java
@@ -1,17 +1,33 @@
 package com.example.demo.controller;
+import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import com.example.demo.model.IDatosUsuario;
+import com.example.demo.service.IUsuarioService;
 
 @Controller
 public class HomeController {
+	
+	@Autowired
+	IUsuarioService servicioDeUsuario;
+	
 	@GetMapping("/login")
 	public String login() {
 		return "login";
 	}
 	
 	@GetMapping("/")
-	public String mostrarHome() {
+	public String mostrarHome(HttpSession session) {
+		if (session.getAttribute("usuario") == null){
+			IDatosUsuario usuario = servicioDeUsuario.datosUsuario(1);	
+			System.out.println("Usuario: " + usuario.getNombre());
+			System.out.println("Perfil: " + usuario.getNombrePerfil());
+			System.out.println("Opción: " + usuario.getNombreOpcion());
+			session.setAttribute("usuario", usuario.getNombre());
+		}
 		return "blank";
 	}
 
