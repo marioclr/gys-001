63285f030a28c63770b96530b1f7714c63900054
alkcode <alek_1604@comunidad.unam.mx>
2022-04-07 13:07:09 -0500

    Modificacion de la vista de dataTables

--- src/main/java/com/example/demo/service/IAdscripcionService.java
+++ src/main/java/com/example/demo/service/IAdscripcionService.java
@@ -3,7 +3,7 @@ package com.example.demo.service;
 import java.util.List;
 
 import com.example.demo.model.Adscripcion;
-import com.example.demo.model.IDatosAdscripcion;
+// import com.example.demo.model.IDatosAdscripcion;
 
 public interface IAdscripcionService {
 
